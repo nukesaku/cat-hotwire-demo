c9c6659e08dbe3d289f79c045320fc6e9a929afa
nukesaku <nextniconico@gmail.com>
2025-01-02 23:32:50 +0900

    編集・登録・削除のコールバックをbroadcasts_toにまとめる

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,7 @@
 class Dog < ApplicationRecord
   validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }
 
-  after_create_commit -> { broadcast_prepend_to("dogs") }
-  after_update_commit -> { broadcast_replace_to("dogs") }
-  after_destroy_commit -> { broadcast_remove_to("dogs") }
+  broadcasts_to -> (_dog) { "dogs" }, inserts_by: :prepend
 
   def self.ransackable_attributes(auth_object = nil)
     ["age", "created_at", "id", "name", "updated_at"]
